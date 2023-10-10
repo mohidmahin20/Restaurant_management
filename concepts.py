b627b34d62df182f60f514f6c92a82a17ed48a08
Mohiduzzaman Mahin <86875694+mohidmahin20@users.noreply.github.com>
2023-10-10 19:14:59 +0600

    Add files via upload

---
+++ concepts.py
@@ -0,0 +1,26 @@
+"""
+1. inheritance
+2. encapsulation
+3. polymorphysm
+4. abstraction
+
+-------------
+classes
+1. User: base class (ABC)
+2. Customer(User)
+3. Employee(User)
+4. Chef (Employee)
+5. Server (Employee)
+6.Cleaner (EMployee)
+7. Supplier (User)
+8. Marketer (User)
+
+1. FoodItem(ABC)
+2. Pizza(FoodItem)
+3. Burger (FoodItem)
+4. Juice (FoodItem)
+5. Salad (FoodItem)
+
+1. Order
+2. Restaurant
+"""
